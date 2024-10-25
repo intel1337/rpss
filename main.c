5e341d44f24230bdfae0a0cd53240015b057b046
intel <93615583+intel1337@users.noreply.github.com>
2024-10-25 11:14:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,80 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "func.c"
 
 #define SHI 1
 #define FU 2
 #define MI 3
 #define MAX_LENGTH 3
+
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+#define BBLK "\e[1;30m"
+#define BRED "\e[1;31m"
+#define BGRN "\e[1;32m"
+#define BYEL "\e[1;33m"
+#define BBLU "\e[1;34m"
+#define BMAG "\e[1;35m"
+#define BCYN "\e[1;36m"
+#define BWHT "\e[1;37m"
+#define UBLK "\e[4;30m"
+#define URED "\e[4;31m"
+#define UGRN "\e[4;32m"
+#define UYEL "\e[4;33m"
+#define UBLU "\e[4;34m"
+#define UMAG "\e[4;35m"
+#define UCYN "\e[4;36m"
+#define UWHT "\e[4;37m"
+#define BLKB "\e[40m"
+#define REDB "\e[41m"
+#define GRNB "\e[42m"
+#define YELB "\e[43m"
+#define BLUB "\e[44m"
+#define MAGB "\e[45m"
+#define CYNB "\e[46m"
+#define WHTB "\e[47m"
+#define BLKHB "\e[0;100m"
+#define REDHB "\e[0;101m"
+#define GRNHB "\e[0;102m"
+#define YELHB "\e[0;103m"
+#define BLUHB "\e[0;104m"
+#define MAGHB "\e[0;105m"
+#define CYNHB "\e[0;106m"
+#define WHTHB "\e[0;107m"
+#define HBLK "\e[0;90m"
+#define HRED "\e[0;91m"
+#define HGRN "\e[0;92m"
+#define HYEL "\e[0;93m"
+#define HBLU "\e[0;94m"
+#define HMAG "\e[0;95m"
+#define HCYN "\e[0;96m"
+#define HWHT "\e[0;97m"
+#define BHBLK "\e[1;90m"
+#define BHRED "\e[1;91m"
+#define BHGRN "\e[1;92m"
+#define BHYEL "\e[1;93m"
+#define BHBLU "\e[1;94m"
+#define BHMAG "\e[1;95m"
+#define BHCYN "\e[1;96m"
+#define BHWHT "\e[1;97m"
+#define reset "\e[0m"
+#define CRESET "\e[0m"
+#define COLOR_RESET "\e[0m"
+
 
 int shi;
 int fu;
@@ -70,14 +134,23 @@ void load_score(char *prenom1, char *prenom2, int *score1, int *score2) {
     fclose(fptr);
 }
 void ascii(){
+    printf("%s", MAG);
     printf("      ::::::::  :::    ::: ::::::::::: :::::::::: :::    :::   :::   :::   :::::::::::\n");
+    printf("%s", CYN);
     printf("    :+:    :+: :+:    :+:     :+:     :+:        :+:    :+:  :+:+: :+:+:      :+:     \n");
+    printf("%s", BLU);
     printf("   +:+        +:+    +:+     +:+     +:+        +:+    +:+ +:+ +:+:+ +:+     +:+      \n");
+    printf("%s", WHT);
     printf("  +#++:++#++ +#++:++#++     +#+     :#::+::#   +#+    +:+ +#+  +:+  +#+     +#+       \n");
+    printf("%s", BLU);
     printf("        +#+ +#+    +#+     +#+     +#+        +#+    +#+ +#+       +#+     +#+        \n");
+    printf("%s", CYN);
     printf("#+#    #+# #+#    #+#     #+#     #+#        #+#    #+# #+#       #+#     #+#         \n");
+    printf("%s", MAG);
     printf("########  ###    ### ########### ###         ########  ###       ### ###########      \n");
     printf("\n");
+    printf("%s" reset);
+
 
 }
 int main() {
